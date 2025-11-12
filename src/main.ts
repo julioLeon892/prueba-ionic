43b0a04d75279ccad40529839d9047f0023a0ffe
Jcleon <100035397+julioLeon892@users.noreply.github.com>
2025-11-12 17:23:56 -0500

    Register Ionicons used in home view

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { addIcons } from 'ionicons';
+import {
+  add as addIcon,
+  bookmark,
+  bookmarkOutline,
+  cloudDoneOutline,
+  cloudOfflineOutline,
+  cloudUploadOutline,
+  createOutline,
+  trashOutline,
+} from 'ionicons/icons';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+addIcons({
+  add: addIcon,
+  bookmark,
+  'bookmark-outline': bookmarkOutline,
+  'cloud-done-outline': cloudDoneOutline,
+  'cloud-offline-outline': cloudOfflineOutline,
+  'cloud-upload-outline': cloudUploadOutline,
+  'create-outline': createOutline,
+  'trash-outline': trashOutline,
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
